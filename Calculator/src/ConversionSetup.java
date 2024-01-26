2eb3afecb1e43e37df8892cdf41167def41e28f6
Chase Manzella <chasewmanzella@gmail.com>
2024-01-26 09:54:32 -0600

    Setup conversion forgiving code

--- Calculator/src/ConversionSetup.java
+++ Calculator/src/ConversionSetup.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class ConversionSetup {
 	//void setup () {
 	Scanner scanner = new Scanner(System.in);
-	
+
+	var forgive = 1;
 	double input;
 	double rslt;
 	int order;
