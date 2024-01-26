7ae953c8d2c8704d81ffe0a5511d45251ebab72c
Chase Manzella <chasewmanzella@gmail.com>
2024-01-26 10:08:55 -0600

    Forgiving code
    Implemented forgiving code.

--- Calculator/src/MiKl.java
+++ Calculator/src/MiKl.java
@@ -3,22 +3,27 @@ public class MiKl extends ConversionSetup {
 	void start () {
 		System.out.println("\n1. Miles to Kilometers\n2. Kilometers to Miles");
 		order = scanner.nextInt();
-		switch (order) {
-		case 1:
-			System.out.println("Input Miles");
-			input = scanner.nextDouble();
-			rslt = ((input * 8) / 5);
-			System.out.println(rslt + " Kilometers");
-			break;
-		case 2:
-			System.out.println("Input Kilometers");
-			input = scanner.nextDouble();
-			rslt = ((input * 5) / 8);
-			System.out.println(rslt + " Miles");
-			break;
-		default:
-			System.out.println("Invalid Command");
-		}
+		while (forgive == 1) {
+			switch (order) {
+			case 1:
+				forgive = 0;
+				System.out.println("Input Miles");
+				input = scanner.nextDouble();
+				rslt = ((input * 8) / 5);
+				System.out.println(rslt + " Kilometers");
+				break;
+			case 2:
+				forgive = 0;
+				System.out.println("Input Kilometers");
+				input = scanner.nextDouble();
+				rslt = ((input * 5) / 8);
+				System.out.println(rslt + " Miles");
+				break;
+			default:
+				forgive = 1;
+				System.out.println("Invalid command. Please choose a valid command. \n");
+			}
+		} 
 	}
 	
 }
