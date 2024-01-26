95e6a92ed7290506b5f3e1eea4df9b9ff3f1a1b3
Chase Manzella <chasewmanzella@gmail.com>
2024-01-26 09:57:59 -0600

    Forgive and void setup
    Forgiving code setup added and made it so the code requires activation from interface to activate.

--- Calculator/src/CalculatorSetup.java
+++ Calculator/src/CalculatorSetup.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
 public class CalculatorSetup {
+	void setupcalc {
 	double x;
 	double y;
 	double z;
+
+	var forgive = 1;
 	// cont means continue
 	String cont = "Y";
 	// rslt means result
 	double rslt;
 	Scanner scanner = new Scanner(System.in);
+	}
 }
