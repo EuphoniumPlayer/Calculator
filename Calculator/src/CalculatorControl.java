52db9e942d6756706cfc4810612fa25d78990e3c
Chase Manzella <chasewmanzella@gmail.com>
2024-01-26 10:17:51 -0600

    Added forgiving code
    Added forgiving code

--- Calculator/src/CalculatorControl.java
+++ Calculator/src/CalculatorControl.java
@@ -7,30 +7,33 @@ public class CalculatorControl {
 	Subtraction sub = new Subtraction();
 	Multiplication mul = new Multiplication();
 	Division div = new Division();
+	static var forgive = 1;
 	
 	void begin () {
 		System.out.println("What operation would you like to do?\n\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
 		int choice = scanner.nextInt();
-		switch (choice) {
-		case 1:
-			System.out.println("Addition Selected\n");
-			add.run();
-			break;
-		case 2:
-			System.out.println("Subtraction Selected\n");
-			sub.run();
-			break;
-		case 3:
-			System.out.println("Multiplication Selected\n");
-			mul.run();
-			break;
-		case 4:
-			System.out.println("Division Selected\n");
-			div.run();
-			break;
-		default:
-			System.out.println("\n\nOperation does not exist");
-			break;
+		while (forgive == 1) {
+			switch (choice) {
+			case 1:
+				System.out.println("Addition Selected\n");
+				add.run();
+				break;
+			case 2:
+				System.out.println("Subtraction Selected\n");
+				sub.run();
+				break;
+			case 3:
+				System.out.println("Multiplication Selected\n");
+				mul.run();
+				break;
+			case 4:
+				System.out.println("Division Selected\n");
+				div.run();
+				break;
+			default:
+				System.out.println("\n\nOperation does not exist, please try again. \n");
+				break;
+			}
 		}
 	}
 	
