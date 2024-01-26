9da6ca82d2ea646d9462bab68794469788934b53
Chase Manzella <chasewmanzella@gmail.com>
2024-01-26 09:52:36 -0600

    Forgiving code pt 2
    More forgiving code, this time to the Celsius to Fahrenheit selection.

--- Calculator/src/CF.java
+++ Calculator/src/CF.java
@@ -1,25 +1,29 @@
-
 public class CF extends ConversionSetup {
-	void start () {
+	
+	static var forgive = 1;
+	
+	void start () {	
 		System.out.println("\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius");
 		order = scanner.nextInt();
-		switch (order) {
-		case 1:
-			System.out.println("Input Celsius");
-			input = scanner.nextDouble();
-			rslt = ((input * 9) / 5) + 32;
-			System.out.println("\n" + rslt + " F");
-			break;
-		case 2:
-			System.out.println("Input Fahrenheit");
-			input = scanner.nextDouble();
-			rslt = ((input - 32) * 5) / 9;
-			System.out.println("\n" + rslt + " C");
-			break;
-		default:
-			System.out.println("Invalid Command");
-			break;
-		}
-	}
+		while (forgive == 1) {	
+			switch (order) {
+			case 1:
+				System.out.println("Input Celsius");
+				input = scanner.nextDouble();
+				rslt = ((input * 9) / 5) + 32;
+				System.out.println("\n" + rslt + " F");
+				break;
+			case 2:
+				System.out.println("Input Fahrenheit");
+				input = scanner.nextDouble();
+				rslt = ((input - 32) * 5) / 9;
+				System.out.println("\n" + rslt + " C");
+				break;
+			default:
+				System.out.println("Invalid command. Please choose a valid command. \n");
+				break;
+			} //close switch
+		} //close while loop
+	} //close void
 	
-}
+} //close code
