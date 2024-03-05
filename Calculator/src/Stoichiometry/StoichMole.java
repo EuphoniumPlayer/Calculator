7106d9ca63d9f49402facbed04c54026978fa42c
Chase Manzella <chasewmanzella@gmail.com>
2024-03-05 10:04:44 -0600

    Fixed oopsie

--- Calculator/src/Stoichiometry/StoichMole.java
+++ Calculator/src/Stoichiometry/StoichMole.java
@@ -506,8 +506,8 @@ public class StoichMole extends StoichiometrySetup {
                                             }
                                         }
                                     case 2: 
-forgive2 = true;
-System.out.println("/n/nSomething went wrong. Please try again/n");
+                                        forgive2 = true;
+                                        System.out.println("/n/nUnder Construction/n");
                                         break;
                                     case 3: 
                                         break;
