5327b24f9dd6260595bd7bc1b2abbfb0de1a1d7f
EuphoniumPlayer <chasewmanzella@gmail.com>
2024-04-16 21:49:06 -0500

    Revert "Completed"
    This reverts commit 6ab105f4eb627cf07420fa88f8f88fcb71d85757.

--- Calculator/src/Setup/StoichiometrySetup.java
+++ Calculator/src/Setup/StoichiometrySetup.java
@@ -3,17 +3,12 @@ package Setup;
 import Tools.Universal;
 
 public class StoichiometrySetup extends Universal {
-    public boolean forgive2 = true;
-    public int type;
-    public int order;
     // Variables for Substance 1
-    public String start;
-    public double startcoe;
-    public double startmol;
-    public double startparts;
+    public String sub1;
+    public double sub1coe;
+    public double sub1mol;
     // Variables for Substance 2
-    public String end;
-    public double endcoe;
-    public double endmol;
-    public double endparts;
+    public String sub2;
+    public double sub2coe;
+    public double sub2mol;
 }
--- Calculator/src/Stoichiometry/StoichControl.java
+++ Calculator/src/Stoichiometry/StoichControl.java
@@ -6,7 +6,6 @@ public class StoichControl extends Universal {
 
     private static int select;
     private static final StoichMoles moles = new StoichMoles();
-    private static final StoichParts parts = new StoichParts();
 
     public void begin () {
         System.out.println("\n\nAvalible Operations: \n\n1. Mole/Moles\n2.Mole/particles\n3. Mole/Mass\n4. Mass/Particles\n");
@@ -16,10 +15,6 @@ public class StoichControl extends Universal {
                 case 1: 
                     forgive = false;
                     moles.go();
-                    break;
-                case 2:
-                    forgive = false;
-                    parts.go();
                     break;
                 default:
                     // invalid
