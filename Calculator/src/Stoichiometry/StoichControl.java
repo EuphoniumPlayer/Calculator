6ab105f4eb627cf07420fa88f8f88fcb71d85757
EuphoniumPlayer <chasewmanzella@gmail.com>
2024-04-16 21:46:32 -0500

    Completed

--- Calculator/src/Stoichiometry/StoichControl.java
+++ Calculator/src/Stoichiometry/StoichControl.java
@@ -6,6 +6,7 @@ public class StoichControl extends Universal {
 
     private static int select;
     private static final StoichMoles moles = new StoichMoles();
+    private static final StoichParts parts = new StoichParts();
 
     public void begin () {
         System.out.println("\n\nAvalible Operations: \n\n1. Mole/Moles\n2.Mole/particles\n3. Mole/Mass\n4. Mass/Particles\n");
@@ -15,6 +16,10 @@ public class StoichControl extends Universal {
                 case 1: 
                     forgive = false;
                     moles.go();
+                    break;
+                case 2:
+                    forgive = false;
+                    parts.go();
                     break;
                 default:
                     // invalid
